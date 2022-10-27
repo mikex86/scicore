02353368c2c942b3e3af583589bdd24ca2f48445
Mike <michael.keiblinger@gmail.com>
2022-10-27 18:16:12 +0200

    Print stacktrace in gradle tests in matplotlib module

--- matplotlib/build.gradle.kts
+++ matplotlib/build.gradle.kts
@@ -35,4 +35,10 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+
+    testLogging {
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showStackTraces = true
+        showCauses = true
+    }
 }
