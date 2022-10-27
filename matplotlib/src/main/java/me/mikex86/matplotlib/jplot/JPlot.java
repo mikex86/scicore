b771d1cbb5341962741e56e47c633ad8d15cc59d
Mike <michael.keiblinger@gmail.com>
2022-10-27 19:49:44 +0200

    Fallback on Arial on JPlot

--- matplotlib/src/main/java/me/mikex86/matplotlib/jplot/JPlot.java
+++ matplotlib/src/main/java/me/mikex86/matplotlib/jplot/JPlot.java
@@ -299,7 +299,7 @@ public class JPlot {
     private static final Paint DEFAULT_NAME_TEXT_PAINT = new Paint();
 
     @NotNull
-    private static final Typeface TYPEFACE = Objects.requireNonNull(FontMgr.getDefault().matchFamilyStyle("Roboto Mono", FontStyle.NORMAL));
+    private static final Typeface TYPEFACE = Objects.requireNonNullElse(FontMgr.getDefault().matchFamilyStyle("Roboto Mono", FontStyle.NORMAL), FontMgr.getDefault().matchFamilyStyle("Arial", FontStyle.NORMAL));
 
     static {
         LINE_PAINT.setStrokeWidth(DEFAULT_STROKE_WIDTH);
