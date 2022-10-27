b64316fced7e0eef48049bec8e36c7e4d9b7916a
Mike <michael.keiblinger@gmail.com>
2022-10-27 18:50:15 +0200

    tmp fix to not build cuda on gh actions for now

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,10 @@
 @file:Suppress("INACCESSIBLE_TYPE")
 import org.gradle.internal.os.OperatingSystem;
+import java.lang.System.getenv
 
 rootProject.name = "scicore"
 include("matplotlib")
-if (OperatingSystem.current() != OperatingSystem.MAC_OS) {
+if (OperatingSystem.current() != OperatingSystem.MAC_OS && !getenv("CI").isNullOrEmpty()) {
     include("cuda-backend")
 }
 include("core")
